cc4b462270d128353052c8daacfc3d7341c14a5b
David Carvajal <davidgcarvajal@gmail.com>
2016-09-07 11:28:15 -0500

    update a la rc6

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import {NavbarComponent} from './components/navbar/navbar.component';
 
 @Component({
     moduleId:module.id,
     selector: 'my-app',
     templateUrl: 'app.component.html',
-    directives: [NavbarComponent]
 })
 export class AppComponent { }
